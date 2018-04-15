aa2bb7977d7f7fe335317f9dd482bb24bab75467
Klara Borowska <klara.borowska@gmail.com>
2018-04-15 20:52:23 +0200

    Change and test the layout

--- js/engine.js
+++ js/engine.js
@@ -24,8 +24,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = 700;
+    canvas.height = 630;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -44,7 +44,7 @@ var Engine = (function(global) {
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
-        update(dt);
+        //update(dt);
         render();
 
         /* Set our lastTime variable which is used to determine the time delta
@@ -77,10 +77,10 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
-    function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
-    }
+    //function update(dt) {
+        //updateEntities(dt);
+        // //checkCollisions();
+    //}
 
     /* This is called by the update function and loops through all of the
      * objects within your allEnemies array as defined in app.js and calls
@@ -89,12 +89,12 @@ var Engine = (function(global) {
      * the data/properties related to the object. Do your drawing in your
      * render methods.
      */
-    function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
-        });
-        player.update();
-    }
+    //function updateEntities(dt) {
+        //allEnemies.forEach(function(enemy) {
+            //enemy.update(dt);
+       // });
+        //player.update();
+    //}
 
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
@@ -107,15 +107,18 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/skyBlock.png',     //row 1 of 4 of sky
+                'images/skyBlock.png',     //row 2 of 4 of sky
+                'images/skyBlock.png',     //row 3 of 4 of sky
+                'images/skyBlock.png',     //row 4 of 4 of sky
+                'images/grassBlock.png',   //row of grass 
+                'images/groundBlock.png',  //row 1 of 3 of ground
+                'images/groundBlock.png',  //row 2 of 3 of ground
+                'images/groundBlock.png',  //row 3 of 3 of ground
+                'images/bottomBlock.png'   //bottom row
             ],
-            numRows = 6,
-            numCols = 5,
+            numRows = 9,
+            numCols = 10,
             row, col;
         
         // Before drawing, clear existing canvas
@@ -134,27 +137,48 @@ var Engine = (function(global) {
                  * so that we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                if (row != numRows - 1) {
+                    ctx.drawImage(Resources.get(rowImages[row]), col * 70, row * 70);
+                } else {
+                    ctx.drawImage(Resources.get(rowImages[row]), col * 70, row * 70 - 6);
+                }
+                
             }
         }
 
-        renderEntities();
+        // testing the layout
+        ctx.drawImage(Resources.get('images/doorOpenSmall.png'), 210, 12);
+        ctx.drawImage(Resources.get('images/boxBlock.png'), 0, 70);
+        ctx.drawImage(Resources.get('images/boxBlock.png'), 70, 70);
+        ctx.drawImage(Resources.get('images/boxBlock.png'), 140, 70);
+        ctx.drawImage(Resources.get('images/boxBlock.png'), 210, 70);
+        ctx.drawImage(Resources.get('images/doorLockedTop.png'), 630, 140);
+        ctx.drawImage(Resources.get('images/doorLockedBottom.png'), 630, 210);
+        ctx.drawImage(Resources.get('images/cloud1.png'), 540, 20);
+        ctx.drawImage(Resources.get('images/cloud2.png'), 350, 100);
+        ctx.drawImage(Resources.get('images/bush.png'), 100, 210);
+        ctx.drawImage(Resources.get('images/bush.png'), 450, 210);
+        ctx.drawImage(Resources.get('images/signRight.png'), 520, 210);
+        ctx.drawImage(Resources.get('images/star.png'), 0, 0);
+
+        //renderEntities();
     }
 
     /* This function is called by the render function and is called on each game
      * tick. Its purpose is to then call the render functions you have defined
      * on your enemy and player entities within app.js
      */
-    function renderEntities() {
+
+    //function renderEntities() {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
+        //allEnemies.forEach(function(enemy) {
+            //enemy.render();
+        //});
 
-        player.render();
-    }
+        //player.render();
+    //}
 
     /* This function does nothing but it could have been a good place to
      * handle game reset states - maybe a new game menu or a game over screen
@@ -169,11 +193,20 @@ var Engine = (function(global) {
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/bottomBlock.png',
+        'images/grassBlock.png',
+        'images/boxBlock.png',
+        'images/skyBlock.png',
+        'images/groundBlock.png',
+        'images/doorOpenSmall.png',
+        'images/doorLockedTop.png',
+        'images/doorLockedBottom.png',
+        'images/cloud1.png',
+        'images/cloud2.png',
+        'images/bush.png',
+        'images/signRight.png',
+        'images/star.png',
+
     ]);
     Resources.onReady(init);
 
